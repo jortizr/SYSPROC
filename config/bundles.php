354745978c687acbb530fdc62be5d1f24c1af2c1
Jefferson Ortiz Rodriguez <jortizr@ut.edu.co>
2024-08-09 17:25:30 -0500

    install easyAdmin 4

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];

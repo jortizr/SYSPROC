54657e38a73403be22f2ed3f45b734b9ade53b6c
Jefferson Ortiz Rodriguez <jortizr@ut.edu.co>
2024-09-25 18:04:30 -0500

    frontend - view human resource refactored

--- assets/app.js
+++ assets/app.js
@@ -7,8 +7,6 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-import './styles/sidebars.css';
 import './js/formLoader';
 import './js/alerts';
 import './styles/customs.scss';
-import './js/sidebars';
